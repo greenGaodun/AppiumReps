d95c9939323ff31e4add84ae119846a9a04487c5
green.hu <green.hu@gaodun.com>
2018-05-31 11:43:07 +0800

    initial version by green

---
+++ num1/src/main/java/com/zdx/appium/num1/App.java
@@ -0,0 +1,13 @@
+package com.zdx.appium.num1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
